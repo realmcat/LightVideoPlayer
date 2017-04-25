b00d6f87f589ffa48ca21e8b7a3aa59fe25c833e
realmcat <realmcat@outlook.com>
2017-04-25 22:13:29 +0800

    Layout

--- LightVideoPlayer/LightVideoPlayer/MainWindow.xaml.cs
+++ LightVideoPlayer/LightVideoPlayer/MainWindow.xaml.cs
@@ -24,5 +24,38 @@ namespace LightVideoPlayer
         {
             InitializeComponent();
         }
+
+        private void TimeUpdate(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            int h, m, s;
+            h = (int)TimeSlider.Value / 3600;
+            m = (int)TimeSlider.Value / 60 % 60; ;
+            s = (int)TimeSlider.Value % 60;
+            labelnowtime.Content = (h < 10 ? "0" + h.ToString() : h.ToString()) +
+                ":" + (m < 10 ? "0" + m.ToString() : m.ToString()) +
+                ":" + (s < 10 ? "0" + s.ToString() : s.ToString()); 
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            TheMedia.Close();
+        }
+
+        private void MediaFailedError(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void PausePlay(object sender, RoutedEventArgs e)
+        {
+            TheMedia.Play();
+        }
+
+        private void MediaOpened(object sender, RoutedEventArgs e)
+        {
+            labelfulltime.Content = TheMedia.NaturalDuration.ToString();
+
+
+        }
     }
 }
